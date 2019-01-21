5176b1838e65521674f0c385a505753eaad52f8d
dinalaith <46858563+dinalaith@users.noreply.github.com>
2019-01-21 08:51:11 +0300

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  bulid(context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Easylist'),
+        ),
+        body:Card (child:Column(children: <Widget>[
+          Image(),
+          Text('platform '),
+
+        ],) ,)
+      ),
+    );
+  }
+}
+
+
+
+test
